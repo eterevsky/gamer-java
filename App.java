80072e791f69511adb73b5edda2110d02a670833
Oleg Eterevsky <olege@google.com>
2014-06-17 01:08:36 +0400

    Use the number of threads matching the number of cores.

--- App.java
+++ App.java
@@ -11,11 +11,13 @@ import java.util.concurrent.ExecutorService;
 
 class App {
   public static void main(String[] args) throws Exception {
+    int cores = Runtime.getRuntime().availableProcessors();
+    System.out.format("Found %d cores.\n", cores);
     ExecutorService executor = Executors.newCachedThreadPool();
 
     GameState<Gomoku> game = Gomoku.newGame();
-    Player player1 = new NaiveMonteCarlo().setExecutor(executor, 6);
-    Player player2 = new NaiveMonteCarlo().setExecutor(executor, 6);
+    Player player1 = new NaiveMonteCarlo().setExecutor(executor, cores);
+    Player player2 = new NaiveMonteCarlo().setExecutor(executor, cores);
 
     System.out.println(game);
 
