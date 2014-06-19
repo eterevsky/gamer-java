ba9668106b7c077cc2381ac6cac364f69b6a9523
Oleg Eterevsky <oleg@eterevsky.com>
2014-06-19 15:48:32 +0400

    Add a couple of tests for GomokuState.

--- gomoku/TestGomokuState.java
+++ gomoku/TestGomokuState.java
@@ -42,4 +42,40 @@ public class TestGomokuState {
     assertTrue(state.isTerminal());
     assertEquals(GameResult.WIN, state.getResult());
   }
+
+  @Test
+  public void playDiagonal1O() {
+    GomokuState state = new GomokuState();
+    state.play(GomokuMove.create('X', 2, 3));
+    state.play(GomokuMove.create('O', 5, 5));
+    state.play(GomokuMove.create('X', 4, 3));
+    state.play(GomokuMove.create('O', 6, 6));
+    state.play(GomokuMove.create('X', 1, 3));
+    state.play(GomokuMove.create('O', 7, 7));
+    state.play(GomokuMove.create('X', 0, 3));
+    state.play(GomokuMove.create('O', 8, 8));
+    state.play(GomokuMove.create('X', 10, 10));
+    state.play(GomokuMove.create('O', 9, 9));
+
+    assertTrue(state.isTerminal());
+    assertEquals(GameResult.LOSS, state.getResult());
+  }
+
+  @Test
+  public void playDiagonal2O() {
+    GomokuState state = new GomokuState();
+    state.play(GomokuMove.create('X', 2, 3));
+    state.play(GomokuMove.create('O', 6, 5));
+    state.play(GomokuMove.create('X', 4, 3));
+    state.play(GomokuMove.create('O', 5, 6));
+    state.play(GomokuMove.create('X', 1, 3));
+    state.play(GomokuMove.create('O', 4, 7));
+    state.play(GomokuMove.create('X', 0, 3));
+    state.play(GomokuMove.create('O', 3, 8));
+    state.play(GomokuMove.create('X', 10, 10));
+    state.play(GomokuMove.create('O', 2, 9));
+
+    assertTrue(state.isTerminal());
+    assertEquals(GameResult.LOSS, state.getResult());
+  }
 }
