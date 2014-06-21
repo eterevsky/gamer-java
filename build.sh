5d41747bb4ae43bb79725881b4ac32670648ea3f
Oleg Eterevsky <oleg@eterevsky.com>
2014-06-21 18:02:19 +0400

    Move all Java sources to a gamer/ directory to match packages.

--- build.sh
+++ build.sh
@@ -5,9 +5,8 @@ rm -f *.jar *.class */*.class
 CLASSPATH=$CLASSPATH:/usr/share/java/junit4.jar
 
 javac -cp .:/usr/share/java/junit4.jar -Xlint:unchecked \
-    def/*.java gomoku/*.java players/*.java treegame/*.java *.java && \
-java -cp ..:.:/usr/share/java/junit4.jar org.junit.runner.JUnitCore \
+    gamer/*.java gamer/*/*.java && \
+java -cp .:/usr/share/java/junit4.jar org.junit.runner.JUnitCore \
     gamer.TestMain && \
-cd .. && \
-jar cvfe gamer/gamer.jar gamer.App \
+jar cvfe gamer.jar gamer.App \
     gamer/App.class gamer/def/*.class gamer/gomoku/*.class gamer/players/*.class
