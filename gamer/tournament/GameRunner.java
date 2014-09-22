070419194da00eeb6426c2b39a30f1f8c0322e5e
Oleg Eterevsky <oleg@eterevsky.com>
2014-09-23 01:00:49 +0400

    Fix syntax error.

--- gamer/tournament/GameRunner.java
+++ gamer/tournament/GameRunner.java
@@ -25,13 +25,14 @@ public final class GameRunner<G extends Game<G>> implements Runnable {
       if (match == null)
         return;
 
-      match.result = playSingleGame(match.game, match.player1, match.player2);
+      match.result = playSingleGame(
+          match.game, match.player1, match.player2, verbose);
       results.add(match);
     }
   }
 
   public static <G extends Game<G>> GameStatus playSingleGame(
-      Game<G> game, Player<G> p1, Player<G> p2) {
+      Game<G> game, Player<G> p1, Player<G> p2, boolean verbose) {
     GameState<G> state = game.newGame();
 
     while (!state.isTerminal()) {
